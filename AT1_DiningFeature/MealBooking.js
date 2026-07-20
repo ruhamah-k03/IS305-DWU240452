03bc67a2234f58b2b80e0a860a1455626bd4be60
ruhamah-k03 <kairatruhamah10@gmail.com>
2026-07-20 20:45:54 +1000

    Add getters and setters

--- AT1_DiningFeature/MealBooking.js
+++ AT1_DiningFeature/MealBooking.js
@@ -16,4 +16,78 @@ class MealBooking {
     #quantity;
     #dietaryNote;
     #bookingStatus;
-}
+
+// 2. Create a constructor that receives booking info
+    constructor(studentId, studentName, mealDate, mealType, quantity, dietaryNote) {
+        this.#studentId = studentId;
+        this.#studentName = studentName;
+        this.#mealDate = mealDate;
+        this.#mealType = mealType;
+        this.#quantity = quantity;
+        this.#dietaryNote = dietaryNote;
+        
+        // 3. Set bookingStatus to Pending by default
+        this.#bookingStatus = "Pending";
+    }
+
+    // 4. Create appropriate getters and setters
+    
+    // Student ID
+    get studentId() {
+        return this.#studentId;
+    }
+    set studentId(value) {
+        this.#studentId = value;
+    }
+
+    // Student Name
+    get studentName() {
+        return this.#studentName;
+    }
+    set studentName(value) {
+        this.#studentName = value;
+    }
+
+    // Meal Date
+    get mealDate() {
+        return this.#mealDate;
+    }
+    set mealDate(value) {
+        this.#mealDate = value;
+    }
+
+    // Meal Type
+    get mealType() {
+        return this.#mealType;
+    }
+    set mealType(value) {
+        this.#mealType = value;
+    }
+
+    // Quantity
+    get quantity() {
+        return this.#quantity;
+    }
+    set quantity(value) {
+        if (value > 0) {
+            this.#quantity = value;
+        } else {
+            console.log("Quantity must be greater than 0.");
+        }
+    }
+
+    // Dietary Note
+    get dietaryNote() {
+        return this.#dietaryNote;
+    }
+    set dietaryNote(value) {
+        this.#dietaryNote = value;
+    }
+
+    // Booking Status
+    get bookingStatus() {
+        return this.#bookingStatus;
+    }
+    set bookingStatus(value) {
+        this.#bookingStatus = value;
+    }}
