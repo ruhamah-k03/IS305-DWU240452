654c533a378b5693a7a1a8af9bbe279f99292a43
ruhamah-k03 <kairatruhamah10@gmail.com>
2026-07-20 20:39:54 +1000

    Create MealBooking Class

---
+++ AT1_DiningFeature/MealBooking.js
@@ -0,0 +1,19 @@
+ /*
+  Program: Dining Meal Booking Feature
+  Student Name: RUHAMAH KAIRAT
+  Student ID: 240452
+  Date: 19 July 2026
+  Description: A JavaScript program demonstrating classes,
+  objects, constructors, private fields and methods.
+*/
+
+class MealBooking {
+    // 1. Declare private fields
+    #studentId;
+    #studentName;
+    #mealDate;
+    #mealType;
+    #quantity;
+    #dietaryNote;
+    #bookingStatus;
+}
